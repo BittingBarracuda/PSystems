170183108475c35e67d85e80491dbffd198e056b
BittingBarracuda <kmartinez2397@gmail.com>
2022-08-24 15:38:57 +0200

    Fixed __compute_num_applications and __rule_blocks implemented in Membrane class.

--- membrane.py
+++ membrane.py
@@ -10,7 +10,8 @@ class Membrane:
         self.universe = list(set([rule.lhs.keys() for rule in self.rules]))
         self.__new_contents = Multiset()
         self.__np_matrix = self.__compute_np_matrix()
-        self.num_applications = np.zeros(shape = (len(self.rules, )))
+        self.__num_applications = np.zeros(shape = (len(self.rules, )))
+        self.__rule_blocks_shape = self.__rule_blocks()
     
     ################# PRIVATE METHODS ###################
     
@@ -27,8 +28,19 @@ class Membrane:
         contents_vector = Multiset.compute_np_vector(self.contents, self.universe)
         aux = contents_vector // self.__np_matrix
         temp_matrix = np.where(aux == np.nan, np.inf, aux)
-        self.num_applications = np.min(temp_matrix, axis = 1).astype(int)
-
+        self.__num_applications = np.min(temp_matrix, axis = 1).astype(int).reshape(self.__rule_blocks_shape)
+    
+    def __rule_blocks(self):
+        priority_list = [x.priority for x in self.rules]
+        current_priority = priority_list[0]
+        temp_array = [[0]]
+        for i in range(1, len(priority_list)):
+            if priority_list[i].priority != current_priority:
+                current_priority = priority_list[i].priority
+                temp_array.append([])
+            temp_array[-1].append(0)
+        return np.array(temp_array).shape
+        
     def __apply_rule(self, rule):
         self.contents = self.contents - rule.lhs
         self.__new_contents = self.__new_contents + rule.rhs
@@ -45,19 +57,19 @@ class Membrane:
     def __sort_rules_by_priority(rule_list):
         return sorted(rule_list, key = lambda x: x.priority)
     
-    def __get_rule_blocks(self):
-        rules_priority = Membrane.__sort_rules_by_priority(self.__get_applicable_rules)
-        if len(rules_priority) > 0:
-            res = [[rules_priority[0]]]
-            current_priority = rules_priority[0].priority
-            for i in range(1, len(rules_priority)):
-                current_rule = rules_priority[i]
-                if current_rule.priority != current_priority:
-                    res.append([])
-                res[-1].append(current_rule)
-            return res
-        else:
-            return []
+    # def __get_rule_blocks(self):
+    #     rules_priority = Membrane.__sort_rules_by_priority(self.__get_applicable_rules)
+    #     if len(rules_priority) > 0:
+    #         res = [[rules_priority[0]]]
+    #         current_priority = rules_priority[0].priority
+    #         for i in range(1, len(rules_priority)):
+    #             current_rule = rules_priority[i]
+    #             if current_rule.priority != current_priority:
+    #                 res.append([])
+    #             res[-1].append(current_rule)
+    #         return res
+    #     else:
+    #         return []
     
     #################### PUBLIC METHODS #########################
 
